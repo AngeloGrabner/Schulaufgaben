d74abf60bdf0cb476eaab0efbad1272f8180ea48
AngeloGrabner <angelo.grabner@gmx.de>
2022-04-07 10:35:42 +0200

    Done

--- Schulaufgaben/Program.cs
+++ Schulaufgaben/Program.cs
@@ -20,34 +20,118 @@ namespace Program //link: https://github.com/AngeloGrabner/Schulaufgaben
     class Aufgaben
     {
         // data
-        const string path = @"Data.txt";
+        const string path = @"..\..\..\Data.txt";
         static List<HotelDaten> daten = new();
         static Dictionary<int, HotelDaten> byNumber = new();
-        static Dictionary<string, HotelDaten> byName = new();
+        static Dictionary<string, HotelDaten> byName = new(); // redundance
         public static void Main()
         {
-            //gui stuff
+            SetupData();
+            while (true)
+            {
+                Console.WriteLine("geben sie eine zimmernummer oder einen nachnamen ein");
+                int inputZimmernummer = 0;
+                string inputStr = @" ";
+                inputStr = Console.ReadLine();
+                if (inputStr == "stop"|| inputStr == "end")
+                {
+                    break;
+                }
+                bool flag = int.TryParse(inputStr, out inputZimmernummer);
+                if (flag)
+                {
+                    (bool b, HotelDaten? hotelDaten) = GetData(inputZimmernummer);
+                    if (b)
+                    {        
+                        Console.WriteLine($"Zimmernummer: {hotelDaten.Value.zimmernummer}, Vorname: {hotelDaten.Value.vorname}, Nachname: {hotelDaten.Value.nachname}, Übernachtungen: {hotelDaten.Value.übernachtungen}, Rechnung; {hotelDaten.Value.rechnung}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+                else
+                {
+                    (bool b, HotelDaten? hotelDaten) = GetData(inputStr);
+                    if (b)
+                    {
+                        Console.WriteLine($"Zimmernummer: {hotelDaten.Value.zimmernummer}, Vorname: {hotelDaten.Value.vorname}, Nachname: {hotelDaten.Value.nachname}, Übernachtungen: {hotelDaten.Value.übernachtungen}, Rechnung; {hotelDaten.Value.rechnung}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+            } 
         }
         static void SetupData()
         {
+            HotelDaten temp = new HotelDaten();
+            int first = 0;
             string[] fromFile = File.ReadAllLines(path);
-            for (int i = 0, j = 0; i < fromFile.Length; i++)
+            for (int i = 0; i < fromFile.Length; i++)
             {
-                while (from[i][j] !=)
+                first = 0;
+                for (int j = 0; j < fromFile[i].Length; j++)
                 {
-                    // get into the hoteldaten struct
+                    if (fromFile[i][j] == ';')
+                    {
+                        temp.zimmernummer = Convert.ToInt32(fromFile[i].Substring(first, j-first));
+                        first = j + 1;
+                    }
+                    else if (fromFile[i][j] == '_')
+                    {
+                        temp.vorname = fromFile[i].Substring(first, j-first);
+                        first = j + 1;
+                    }
+                    else if (fromFile[i][j] == '!')
+                    {
+                        temp.nachname = fromFile[i].Substring(first, j-first);
+                        first = j +1;
+                    }
+                    else if (fromFile[i][j] == '|')
+                    {
+                        temp.übernachtungen = Convert.ToInt32(fromFile[i].Substring(first, j-first));
+                        first = j + 1;
+                    }
+                    else if (fromFile[i][j] == '\n')
+                    {
+                        temp.rechnung = (float)Convert.ToDouble(fromFile[i].Substring(first, j-first));
+                        first = j + 1;
+                    }
                 }
+                daten.Add(temp);
+            }
+            for (int i = 0; i < daten.Count; i++)
+            {
+                byName.Add(daten[i].nachname, daten[i]);
+                byNumber.Add(daten[i].zimmernummer,daten[i]);
             }
         }
-        public static Tuple<bool, HotelDaten>GetData(string nachname)
+        public static Tuple<bool, HotelDaten?>GetData(string nachname)
         {
+            try
+            {
+                HotelDaten temp = byName[nachname];
+                return new(true,temp);
+            }
+            catch
+            {
+                return new(false,null);
+            }
 
-            return null;
         }
-        public static Tuple<bool, HotelDaten>GetData(int zimmernummer)
+        public static Tuple<bool, HotelDaten?>GetData(int zimmernummer)
         {
-
-            return null;
+            try
+            {
+                HotelDaten temp = byNumber[zimmernummer];
+                return new(true, temp);
+            }
+            catch
+            {
+                return new(false, null);
+            }
         }
     }
 }
